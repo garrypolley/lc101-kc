132172b4bc1c1eeca832dbc18ba680e33ca69cba
Garry Polley <garrympolley@gmail.com>
2017-03-04 08:08:09 -0600

    add studio soluiton

---
+++ March 02, 2017/code/area/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Area
+    {
+        public static void Main(string[] args)
+        {
+            double radius;
+
+            Console.Write("Enter a radius: ");
+            radius = double.Parse(Console.ReadLine());
+
+            double area;
+
+            area = Math.PI * Math.Pow(radius, 2);
+
+            string message = "The area of a circle with radius {0} is: {1:0.###}";
+
+            Console.WriteLine(string.Format(message, radius, area));
+        }
+    }
+}
