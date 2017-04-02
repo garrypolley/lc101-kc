accf854690ddda9129ade1fd53dd80399f55cfc2
Garry Polley <garrympolley@gmail.com>
2017-04-02 17:21:28 -0500

    Add stuff form march 30th

---
+++ March 30, 2017/code/exception-example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exception_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add number together");
+
+            Console.Write("First Number: ");
+            var numberOneStr = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Second Number: ");
+            var numberTwoStr = Console.ReadLine();
+
+            int numberTwo;
+            try {
+                numberTwo = int.Parse(numberTwoStr);
+            } catch (FormatException) {
+                numberTwo = 0;
+            }
+
+            int numberOne;
+            try {
+                numberOne = int.Parse(numberOneStr);
+            } catch (FormatException) {
+                numberOne = 0;
+            }
+
+
+            var sumNumbers =  numberOne + numberTwo;
+
+            Console.WriteLine();
+            Console.WriteLine($"{sumNumbers} Done");
+            Console.Read();
+        }
+    }
+}
---
+++ March 30, 2017/talking-points.md
@@ -0,0 +1,23 @@
+# Talking points
+
+## Before Class
+
+* Start Youtube live
+
+## Start of Class
+
+* Mention the new location of the class:
+  * Westport Commons: 300 E. 39th St., KCMO (39th @ Warwick / Midtown) Class Location: April 3, 6, 10, 13
+  * Location: https://goo.gl/rnMzwv
+
+## Lecture
+
+* Cover stuff form TF groups
+* Go over studio solution: git@github.com:LaunchCodeEducation/UserSignup.git
+  * Go to the web page first: http://localhost:5000/User/Add
+* Go over exceptions and the base parts of them: https://msdn.microsoft.com/en-us/library/z4c5tckx.aspx
+* Write a tiny command line program to do reading in of numbers to handle the exceptions
+* Show demo app: http://techjobsoo.azurewebsites.net/
+* Talk through to "todos" of the job oo assignment
+
+## End of Class
